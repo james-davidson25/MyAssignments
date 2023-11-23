c6ea48b72ab27ebf0218f7d4f8adc88136b32346
james-davidson25 <151315077+james-davidson25@users.noreply.github.com>
2023-11-23 19:36:44 +0530

    Add files via upload
    Day1

---
+++ day1/Firefox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Firefox {
+	
+   public static void main(String[] args) {
+	   
+	   float browserVersion = 100.2f;
+	   String browserName = "firefox";
+	   boolean isVisible = true;
+	   int releaseYear = 1988;
+	   char browserLogo = 'f';
+	   
+	System.out.println("Browser Name:" +browserName);
+	System.out.println("Browser Version:" +browserVersion);
+	System.out.println("Visible:" +isVisible);
+	System.out.println("Year of Release:" +releaseYear);
+	System.out.println("Browser Logo:" +browserLogo);
+	
+}
+   
+}
